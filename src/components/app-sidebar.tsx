adc655d3bb0cf71382b943bf62cbb39b897747f8
gideonadeti <gideonadeti0@gmail.com>
2024-11-02 20:29:15 +0000

    Set up sidebar

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -3,16 +3,54 @@ import {
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
-  SidebarHeader,
+  SidebarGroupLabel,
+  SidebarGroupContent,
 } from "@/components/ui/sidebar";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+const weatherUnits = [
+  {
+    name: "TEMPERATURE",
+    options: ["Celsius (°C)", "Fahrenheit (°F)"],
+  },
+  {
+    name: "WIND SPEED",
+    options: ["km/h", "mph"],
+  },
+  {
+    name: "PRESSURE",
+    options: ["mb", "in"],
+  },
+  {
+    name: "PRECIPITATION",
+    options: ["Millimeters (mm)", "Inches (in)"],
+  },
+  {
+    name: "VISIBILITY",
+    options: ["Kilometers (km)", "Miles (mi)"],
+  },
+];
 
 export function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarHeader />
       <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
+        {weatherUnits.map((unit) => (
+          <SidebarGroup key={unit.name}>
+            <SidebarGroupLabel>{unit.name}</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <Tabs defaultValue={unit.options[0]}>
+                <TabsList>
+                  {unit.options.map((option) => (
+                    <TabsTrigger key={option} value={option}>
+                      {option}
+                    </TabsTrigger>
+                  ))}
+                </TabsList>
+              </Tabs>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        ))}
       </SidebarContent>
       <SidebarFooter />
     </Sidebar>
