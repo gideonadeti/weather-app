7f35a6e450e605cfb4a25fe673e3c8e83e14fabe
gideonadeti <gideonadeti0@gmail.com>
2024-11-03 15:52:24 +0000

    Use remotePatterns as previous method was deprecated

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["cdn.weatherapi.com"],
-  }
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.weatherapi.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
