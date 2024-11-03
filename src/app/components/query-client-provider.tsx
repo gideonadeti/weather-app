63eee94f8fbd7e33cc734a2f2c8e61d72176bfed
gideonadeti <gideonadeti0@gmail.com>
2024-11-03 17:10:47 +0000

    Prevent refetch on window focus

--- src/app/components/query-client-provider.tsx
+++ src/app/components/query-client-provider.tsx
@@ -3,7 +3,13 @@
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 export default function QCProvider({
   children,
